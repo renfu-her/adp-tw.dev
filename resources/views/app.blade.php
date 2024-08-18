a4979612ed723d10899a9e7bc5d7bb351c634fc2
Patrick Her <renfu.her@gmail.com>
2024-08-18 09:54:12 +0800

    chore: Update app.blade.php with new meta tags, scripts, and stylesheets

---
+++ resources/views/app.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="icon" href="/logo.png">
+    <title>昶安奈米應用科技有限公司</title>
+    <script defer="defer" src="/js/chunk-vendors.d6ebf372.js"></script>
+    <script defer="defer" src="/js/app.84982f63.js"></script>
+    <link href="/css/chunk-vendors.ffa8cdd3.css" rel="stylesheet" />
+    <link href="/css/app.8265b190.css" rel="stylesheet" />
+</head>
+
+<body><noscript><strong>We're sorry but case-boost-vue doesn't work properly without JavaScript enabled. Please enable
+            it to continue.</strong></noscript>
+    <div id="app"></div>
+</body>
+
+</html>
